0c0638d7fb0315fcb2bffb73b6e96d02a084065a
Javid <Javid@DESKTOP-3HBTQMT>
2024-02-14 14:04:43 +0400

    Add project files.

---
+++ SystemProgramming_Intro/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SystemProgramming_Intro;
+
+public partial class MainWindow : Window
+{
+    public List<Proces> processes { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        processes = new List<Proces>();
+
+        foreach (var item in Process.GetProcesses())
+        {
+            Proces p = new Proces();
+
+            p.Name = item.ProcessName;
+            p.Id = item.Id;
+            p.MachineName = item.MachineName;
+            p.ThreadsCount = item.Threads.Count;
+
+            processes.Add(p);
+        }
+
+        listview1.ItemsSource = processes;
+    }
+
+    private void endtask_btn_Click(object sender, RoutedEventArgs e)
+    {
+        Proces proces = listview1.SelectedItem as Proces;
+
+        if (proces != null)
+        {
+            foreach (var item in Process.GetProcesses())
+            {
+                if (item.ProcessName == proces.Name && item.Id == proces.Id && item.MachineName == proces.MachineName && item.Threads.Count == proces.ThreadsCount)
+                {
+                    item.Kill();
+                }
+            }
+        }
+
+        else
+        {
+            MessageBox.Show("Proccess'i sec");
+        }
+    }
+}
---
+++ SystemProgramming_Intro/Proces.cs
@@ -0,0 +1,14 @@
+﻿namespace SystemProgramming_Intro;
+
+public class Proces
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? MachineName { get; set; }
+    public int ThreadsCount { get; set; }
+
+    public Proces()
+    {
+
+    }
+}
